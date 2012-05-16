cd61341e103dabafdee104a29d9faf12c12eb28c
Sebastián Ventura <lomegor@gmail.com>
2012-05-17 00:30:28 +0100

    Version 0.9

--- freelancehours.py
+++ freelancehours.py
@@ -1,6 +1,9 @@
+import logging
 import cgi
 import os
 import datetime
+import urllib
+import sys
 
 
 from google.appengine.api import users
@@ -8,51 +11,139 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from google.appengine.ext import db
-       
+
 class MainPage(webapp.RequestHandler):
 
     def get(self):
         dataHours = []
+        projects = []
+
+        cs = Counter.all()
+        for c in cs:
+            jh = JobHours.all().filter('name =',c.name).get()
+            td = datetime.datetime.now()-c.start
+            tmp = datetime.datetime.combine(datetime.date.today(), jh.hours) + td
+            tmp = tmp.time()
+
+            jh.hours = tmp.replace(microsecond = 0)
+            jh.put()
+
+            c.start = datetime.datetime.now()
+            c.put()
+
         for j in JobHours.all():
             dataHours.append(DataJobHours(j))
 
+        for p in Projects.all():
+            projects.append(p.name)
+
         template_values = {
-                'hours':dataHours
+            'projects':projects,
+            'hours':dataHours
         }
+
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
+
+class Counter(db.Model):
+    name = db.StringProperty()
+    start = db.DateTimeProperty(auto_now=True)
 
 class DataJobHours():
     name = ''
     hours = 0
+    descr = ''
     project = ''
+    started = ''
     def __init__(self,jh):
         self.name = jh.name
-        self.hours = jh.hours
+        self.hours = jh.hours.isoformat()
+        self.descr = jh.descr
         self.project = jh.project.name
-
-class JobHours(db.Model,webapp.RequestHandler):
-    name = db.StringProperty()
-    hours = db.FloatProperty()
-
-    def get(self,n=''):
-        method = self.request.get('method')
-        jh = JobHours.all().filter('name =',n).get()
-        if (method=='update'):
-            hours = self.request.get('hours')
-            jh.hours=hours
-            jh.put()
-        elif (method=='delete'): 
-            db.delete(jh.key())
+        if jh.started==1:
+            self.started='stop'
         else:
-            self.response.out.write(jh.hours)
+            self.started='start'
 
-    def post(self):
-        jh = JobHours(name=self.request.get('name'),hours=0.0)
-        jh.put()
+class Projects(db.Model):
+    name = db.StringProperty()
 
-application = webapp.WSGIApplication([('/', MainPage),
-                                      ('/jh/', JobHours)],
+class JobHours(db.Model):
+    name = db.StringProperty()
+    hours = db.TimeProperty()
+    descr = db.StringProperty()
+    project = db.ReferenceProperty(Projects)
+    started = db.IntegerProperty()
+
+class API(webapp.RequestHandler):
+    def get(self,p='',n=''):
+        try:
+            p = urllib.unquote(p)
+            n = urllib.unquote(n)
+
+            pj = Projects.all().filter('name =',p).get()
+            if not pj:
+                pj = Projects(name = p)
+                pj.put()
+
+            new = False
+            jh = pj.jobhours_set.filter('name =',n).get()
+            if not jh:
+                descr = self.request.get('descr')
+                jh = JobHours(name = n, hours = datetime.time(), descr = descr, project = pj.key())
+                jh.put()
+                new = True
+
+            method = self.request.get('method')
+            if method == 'update':
+                hours = self.request.get('hours')
+                jh.hours = hours
+                jh.put()
+                Respond(self,0,"");
+            elif method == 'delete':
+                c = Counter.all().filter('name =',n)
+                for c1 in c:
+                    db.delete(c1.key())
+                db.delete(jh.key())
+                Respond(self,0,"");
+            elif method == 'start':
+                c = Counter.all().filter('name =',n)
+                for c1 in c:
+                    db.delete(c1.key())
+                c = Counter()
+                c.name = n
+                c.put()
+                jh = JobHours.all().filter('name =',n).get()
+                jh.started=1
+                jh.put()
+                Respond(self,0,"");
+            elif method == 'stop':
+                c = Counter.all().filter('name =',n).get()
+                if c!=None:
+                    jh = JobHours.all().filter('name =',n).get()
+                    jh.started=0
+                    td = datetime.datetime.now()-c.start
+                    tmp = datetime.datetime.combine(datetime.date.today(), jh.hours) + td
+                    tmp = tmp.time()
+                    jh.hours = tmp.replace(microsecond = 0)
+                    jh.put()
+                    c = Counter.all().filter('name =',n)
+                    for c1 in c:
+                        db.delete(c1.key())
+                    Respond(self,0,jh.hours.isoformat())
+            elif new:
+                Respond(self,0,jh.hours.isoformat())
+            else:
+                Respond(self,1,"Task already exists!");
+        except:
+            Respond(self,1,"Unexpected error!")
+            logging.exception("Error")
+
+def Respond(req, error, msg):
+    req.response.out.write("{\"error\":"+str(error)+",\"data\":\""+str(msg)+"\"}")
+
+application = webapp.WSGIApplication([(r'^/$', MainPage),
+                                      (r'/(.*)/(.*)', API)],
                                      debug=True)
 
 def main():
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,179 @@
 $(document).ready(function() {
+	$("#project").change(changeIfNew);
+	$("#header form").submit(newTask);
+	$(document).on('click','.starter',start);
+	$(document).on('click','.stoper',stop);
+	$(document).on('click','.deleter',remove);
+	$('.stoper').each(function() {
+		var $tr = $(this).parent().parent();
+		var $that = $(this);
+		var p = $tr.find(".project").text();
+		var n = $tr.find(".name").text();
+		var h = $tr.find(".hour");
+		if (!counter[p]) {
+			counter[p] = {};
+		}
+		counter[p][n] = 1;
+		setTimeout(function(){count(h,p,n)},1000);
+	});
 });
+
+var newProject = 0;
+
+var counter = {};
+
+function count(e,p,n) {
+	if (counter[p] && counter[p][n] && counter[p][n]==1) {
+		var matches = /(\d\d):(\d\d):(\d\d)/.exec(e.text())
+		var h = parseInt(matches[1],10);
+		var m = parseInt(matches[2],10);
+		var s = parseInt(matches[3],10);
+		if (s == 59) {
+			s = 0;
+			if (m == 59) {
+				m = 0;
+				h+=1;
+			} else {
+				m+=1;
+			}
+		} else {
+			s+=1;
+		}
+		h<10 && (h="0"+h);
+		m<10 && (m="0"+m);
+		s<10 && (s="0"+s);
+		e.text(h+":"+m+":"+s);
+		setTimeout(function(){count(e,p,n)},1000);
+	} else if (counter[p] && counter[p][n]) {
+		delete counter[p][n];
+	}
+};
+
+function start() {
+	$(".stoper").click();
+	$(this).removeClass("starter");
+	$(this).addClass("stoper");
+	$(this).text("Stop");
+	var $tr = $(this).parent().parent();
+	var $that = $(this);
+	var p = $tr.find(".project").text();
+	var n = $tr.find(".name").text();
+	var h = $tr.find(".hour");
+	$.getJSON(
+		'/'+p+'/'+n,
+		{method:"start"},
+		function (data) {
+			if (data.error == 1) {
+				error(data.data);
+				$that.removeClass("stoper");
+				$that.addClass("starter");
+				$that.text("Start");
+				return;
+			}
+		});
+	if (!counter[p]) {
+		counter[p] = {};
+	}
+	counter[p][n] = 1;
+	setTimeout(function(){count(h,p,n)},1000);
+};
+
+function stop() {
+	$(this).removeClass("stoper");
+	$(this).addClass("starter");
+	$(this).text("Start");
+	var $tr = $(this).parent().parent();
+	var $that = $(this);
+	var p = $tr.find(".project").text();
+	var n = $tr.find(".name").text();
+	var h = $tr.find(".hour");
+	$.getJSON(
+		'/'+p+'/'+n,
+		{method:"stop"},
+		function (data) {
+			if (data.error == 1) {
+				error(data.data);
+				$that.removeClass("starter");
+				$that.addClass("stoper");
+				$that.text("Stop");
+				return;
+			}
+			h.text(data.data);
+		});
+	if (counter[p] && counter[p][n]) {
+		counter[p][n] = 0;
+	}
+};
+
+function remove() {
+	var $td = $(this).parent();
+	var $tr = $td.parent();
+	var p = $tr.find(".project").text();
+	var n = $tr.find(".name").text();
+	$td.html("");
+	$.getJSON(
+		'/'+p+'/'+n,
+		{method:"delete"},
+		function (data) {
+			if (data.error == 1) {
+				error(data.data);
+				return;
+			}
+			$tr.remove();
+		});
+};
+
+function changeIfNew(evt) {
+	if ($(this).val() == 'new') {
+		$(this).hide();
+		$("#newproject").show();
+		newProject = 1;
+	};
+};
+
+function newTask(evt) {
+	evt.preventDefault();
+	var p;
+	if (newProject) {
+		p = $("#newproject").val();
+	} else {
+		p = $("#project").val();
+	}
+	if (p == 'all' || p == 'new') {
+		error("No way to create task for All");
+		return false;
+	};
+	var n = $("#newtask").val();
+	var d = $("#newdescr").val();
+	$.getJSON(
+		'/'+p+'/'+n,
+		{descr:d},
+		function (data) {
+			if (data.error == 1) {
+				error(data.data);
+				return;
+			}
+			if (newProject) {
+				$("#project").append($("<option value=\""+p+"\">"+p+"</option>"));
+				$("#newproject").hide();
+				$("#project").val(p);
+				$("#project").show();
+				newProject = 0;
+			}
+			$("#tasks").append($("<tr>"+
+					"<td><button class=\"changer starter\" value=\"start\">Start</button></td>"+
+					"<td class=\"name\">"+n+"</td>"+
+					"<td class=\"project\">"+p+"</td>"+
+					"<td class=\"descr\">"+d+"</td>"+
+					"<td class=\"hour\">"+data.data+"</td>"+
+					"<td><button class=\"deleter\" value=\"delete\">X</button></td>"+
+				"</tr>")).find('.starter').click();
+		}
+	);
+	return false;
+};
+
+function error(string) {
+	$("#error").text(string);
+	setTimeout(function() {$("#error").text("")},5000);
+};
